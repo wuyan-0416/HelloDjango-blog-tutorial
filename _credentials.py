f1932872a26f8831a84f814aac84f29780445cc7
wuyan3 <54521359+wuyan-0416@users.noreply.github.com>
2022-11-11 17:04:33 +0800

    Add files via upload

---
+++ _credentials.py
@@ -0,0 +1,8 @@
+# -*- codeing = utf-8 -*-
+# @Time  :2022/11/11 16:53
+# @Author:wuyan
+# @File  :_credentials.py
+# @Software :PyCharm
+
+github_username = 'wuyan-0416'
+github_password = 'Zx101647'
